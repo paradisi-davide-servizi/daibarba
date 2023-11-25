78a5dfff1d4e54a4fa615e9a8f2ab795ff6cbc06
davide@modulo3d.co <paradisi.davide@gmail.com>
2023-11-25 02:22:10 +0100

    TRY FIX FILE ISSUE

--- src/lib/utils/zodUtils.d.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export function file(maxSize: number) {
-	return z.instanceof(File)
-		.refine(file => file.size <= maxSize, `Max size is ${Math.floor(maxSize / 1024 / 1024)}mb`)
-}
-
-export as namespace zUtils;
