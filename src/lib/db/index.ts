bcabeae2d9b33819e287b426a83ae8fc1c0a4d8a
davide@modulo3d.co <paradisi.davide@gmail.com>
2024-04-14 23:02:33 +0200

    test

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,8 +2,6 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import { registerService } from '../utils/serviceUtils';
 
-export const db = registerService("db", () => {
-    const connectionString = process.env.DATABASE_URL!
-    const client = postgres(connectionString)
-    return drizzle(client);
-})
+const connectionString = process.env.DATABASE_URL!
+const client = postgres(connectionString)
+export const db = drizzle(client);
